f4d2c7a308827c1469b3e2e2f3fae52b4486f1f1
Rob Drimmie <rob.drimmie@gmail.com>
2011-06-12 21:26:52 -0400

    First commit, includes a trivial amount of work.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seymour::Application.config.secret_token = 'dfcb434eba8cd4cda62683f449053c0ad266181a541b33c4e25e73499d6a9a5991a6b26832ac9a04442cdf20d4c5a33e7cb8baf083763fb0d89a222364591dd7'
